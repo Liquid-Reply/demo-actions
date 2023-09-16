b838c67d4480623a9d80f8c9618144c792542d43
Rufaida Mugalli <rmugalli@Liquids-MacBook-Pro.local>
2023-09-16 22:09:02 +0200

    refactor

--- infracost/entrypoint.sh
+++ infracost/entrypoint.sh
@@ -10,7 +10,7 @@ if [ -z "$DEPLOYMENT_ENV" ]; then
   echo "No workspace provided"
   exit 1
 fi
-git diff --name-only origin/"$DEFAULT_BRANCH" >$filename
+git diff --name-only origin/main >$filename
 DIRECTORIES=$(awk -F '/' '{print $1}' text.txt | sort | uniq | grep -v .github | grep -v action-infracost | grep -v infracost.json | grep -v _deprecated |grep -v README.md)
 if [ -z "$DIRECTORIES" ]; then
   echo "Skipping the infra cost analysis as there are no changes to terraform associated files."
